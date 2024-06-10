d2e1a5cf90e4156b5065bb62454d3f3cb7337641
Kimjunwoo <wnsdn123931@gmail.com>
2024-06-10 18:59:04 +0900

    Fix GameOverUI

--- BiteTheBait/Assets/Scripts/GM/GameManagerScript.cs
+++ BiteTheBait/Assets/Scripts/GM/GameManagerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a70313bb7e230c491b6ed393343d3adc93fac259cb17a06dc6f475c3ffa362e3
-size 1384
+oid sha256:dc7b09398ba0c6ca89cc6e0585f3f1c8e1cec99cd63008b78f5afad05294f30f
+size 1487
